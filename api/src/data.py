631765eb84516acdd54063d78e9c185b189f8471
Eduard Frlicka <edo.frlicka@gmail.com>
2022-10-29 21:22:35 +0200

    data update

--- api/src/data.py
+++ api/src/data.py
@@ -1,37 +1,58 @@
+from datetime import date
 import db
-import main
 
-roles = [
-    db.user_role(id=0,name="admimnistrator", description="""
-            - has highest permissions"""),
+role_admin = db.User_role(name="administrator", description="""
+            - has highest permissions""")
 
-    db.user_role(id=1,name="caretaker", description="""
+role_caretaker = db.User_role(name="caretaker", description="""
             - manages animals
             - creates walking timetables
             - verifies volunteers
             - approves animal bookings
-            - creates veterinarian requests"""),
+            - creates veterinarian requests""")
 
-    db.user_role(id=2,name="veterinarian", description="""
+role_vet = db.User_role(name="veterinarian", description="""
             - has access to medical records, 
-            - performs medical procedures"""),
+            - performs medical procedures""")
 
-    db.user_role(id=3,name="volunteer", description="""
+role_volunteer = db.User_role(name="volunteer", description="""
             - can book animal for a walk 
-            - can see his history"""),
+            - can see his history""")
+
+user_roles = [
+    role_admin,
+    role_caretaker,
+    role_vet,
+    role_volunteer,
+]
+
+animals = [
+    db.Animal("Gutenberg", "male", "brown", 15, 40, "dog ?", "dog with 4 legs", 15,
+              date(2015, 12, 2), date(2019, 1, 2), "Bozetechova 2", "It is a dog."),
+]
+
+admin_users = [
+    db.User("admin", "tryhards", "martin", "kneslik", "Berkova 54",
+            "m.kneslik@utulek.cz", "+420603334197", 10, True),
 ]
 
 
 def add_data():
-    for role in roles:
-        main.db.session.add(role)
+    for role in user_roles:
+        db.db.session.add(role)
 
-    main.db.session.commit()
+    for admin in admin_users:
+        admin.user_role = role_admin
+        db.db.session.add(admin)
+
+    # must be last, commits all changes to database
+    db.db.session.commit()
 
 
 if __name__ == "__main__":
-    with main.app.app_context():
-        main.db.drop_all()
-        main.db.create_all()
-        
+    import app
+    with app.app.app_context():
+        db.db.drop_all()
+        db.db.create_all()
+
         add_data()
