cf0fea992f89dbfbed3a5a3f43493f03c5a1f168
annus <nna.krcalova@seznam.cz>
2022-11-01 10:35:52 +0100

    edited language, added animals
    diakritika mainly

--- api/src/data.py
+++ api/src/data.py
@@ -32,48 +32,53 @@ user_roles = [
 ]
 
 animals = [
-    db.Animal("Gutenberg", "male", "brown", 15, 40, "dog", "dog with 4 legs", 15000,
-              date(2015, 12, 2), date(2019, 1, 2), "Bozetechova 2", "Je to pes."),
+    db.Animal("Gutenberg", "samec", "hnědá", 15, 40, "pes", "Kříženec", 15000,
+              date(2015, 12, 2), date(2019, 1, 2), "Božetechova 2", "Hravý a přátelský pes nalezen uvázaný na přednášce IIS, doživotní trauma."),
 
-    db.Animal("Salatek", "male", "black", 40, 80, "dog", "dobrman", 15001,
-              date(2020, 12, 6), date(2022, 1, 2), "Bozetechova 2", "Velky cerny pes."),
+    db.Animal("Salátek", "samec", "černá", 40, 80, "pes", "Dobrman", 15001,
+              date(2020, 12, 6), date(2022, 1, 2), "Kolejní 2", "Velký černý pes, který má rád dlouhé procházky."),
 
-    db.Animal("Mrkvicka", "female", "white", 5, 30, "cat", "ragdoll", 15002,
-              date(2021, 2, 2), date(2022, 1, 2), "Bozetechova 2", "Agresivni mala kocka."),
+    db.Animal("Mrkvička", "samice", "bílá", 5, 30, "kočka", "Ragdoll", 15002,
+              date(2021, 2, 2), date(2022, 1, 2), "Purkyňova 93", "Agresivní malá kočka s alergií na ryby."),
     
-    db.Animal("Oscar", "male", "blue", 0.01, 5, "fish", "bojovka", 15003,
-              date(2022, 2, 8), date(2022, 10, 2), "Bozetechova 2", "Divna velka rybka."),
+    db.Animal("Oscar", "samec", "modrá", 0.01, 5, "ryba", "Bojovka", 15003,
+              date(2022, 2, 8), date(2022, 10, 2), "Mánesova 2524/12", "Rybka většího vzrůstu, musí být v akvárku sama."),
 
-    db.Animal("Rozarka", "female", "tabby", 3, 35, "cat", "unknown", 15004,
-              date(2015, 12, 2), date(2019, 1, 2), "Bozetechova 2", "Kotatko nalezene v odpadcich."),
+    db.Animal("Rozárka", "samice", "barevná", 3, 35, "kočka", "Kříženec", 15004,
+              date(2015, 12, 2), date(2019, 1, 2), "Kounicova 46/48", "Mourovaté koťátko nalezené v odpadcích."),
 
-    db.Animal("Jurko", "male", "pink", 20, 65, "pig", "miniature pig", 15005,
-              date(2021, 12, 2), date(2022, 10, 2), "Bozetechova 2", "Male smradlave prasatko.")
-    # TODO add animals
+    db.Animal("Jurko", "samec", "růžová", 20, 65, "prase", "miniaturní prasátko", 15005,
+              date(2021, 12, 2), date(2022, 10, 2), "Božetěchova 2", "Malé smradlavé prasátko, nemá rádo koupel."),
+
+    db.Animal("Alex", "samec", "šedá", 50, 90, "pes", "Doga", 15006,
+              date(2020, 12, 2), date(2022, 1, 2), "Božetechova 2", "Milý pes většího vzrůstu, vhodný k dětem."),
+
+    db.Animal("Punťa", "samec", "hnědá", 5, 20, "pes", "Čivava", 15007,
+              date(2010, 12, 2), date(2019, 1, 2), "Božetechova 2", "Psí důchodce, který by rád dožil svůj život v milující rodině."),
+
+    db.Animal("Rex", "samec", "zlatá", 25, 60, "pes", "Zlatý retrívr", 15008,
+              date(2022, 9, 2), date(2022, 10, 30), "Božetechova 2", "Štěně bez základního výcviku, které potřebuje pravidelný kontakt s lidmi.")
 ]
 
-admin_user = db.User("admin", "tryhards", "martin", "kneslik", "Berkova 54",
+admin_user = db.User("admin", "tryhards", "Martin", "Kneslik", "Berkova 54",
                      "m.kneslik@utulek.cz", "+420603334197", 10, True)
 
-vet_user = db.User("vet", "treehards", "martin", "kneslik", "Berkova 54",
+vet_user = db.User("vet", "treehards", "Martin", "Kneslík", "Berkova 54",
                    "m.kneslik@utulek.cz", "+420603334197", 10, True)
 
-caretaker_user = db.User("caretaker", "1234", "Ahmed", "Prdik", "Revolucni 1820",
+caretaker_user = db.User("caretaker", "1234", "Ahmed", "Prdík", "Revoluční 1820",
                     "ahm.prd@utulek.cz", "+420721059221", 10, True)
 
-volunteer_user = db.User("volunteer", "abcd", "Jolanda", "Velika", "Breberkova 1420",
+volunteer_user = db.User("volunteer", "abcd", "Jolanda", "Veliká", "Breberkova 1420",
                    "jolis.velis@utulek.cz", "+420603674100", 9, True)
 
-unverified_user = db.User("unverified", "poop", "Juraj", "Prdelkovy", "Vysoke Tatry 4",
+unverified_user = db.User("unverified", "poop", "Juraj", "Prdelkový", "Vysoké Tatry 4",
                    "jurik.prdelka@utulek.cz", "+421913677100", 8, False)
 
-
-# TODO add more users
-
 event_types = [
-    db.event_type("prochazka", 1, "Vziti zvirete mimo utulek s povinnosti navraceni."),
-    db.event_type("vysetreni", 9, "Vysetreni zvirete odbornym pracovnikem.")
-    # TODO more add event_types
+    db.event_type("procházka", 1, "Vzití zviřete mimo útulek s povinnosti navrácení v předem určený čas."),
+    db.event_type("vyšetření", 9, "Vyšetrení zvířete odborným pracovníkem (veterinářem)."),
+    db.event_type("salón", 5, "Návštěva psího salónu s cílem vylepšení vzhledu zvířete (koupel, stříhání srsti, stříhání drápků).")
 ]
 
 events = [
@@ -97,7 +102,6 @@ def add_data():
     volunteer_user.user_role = role_volunteer
     db.db.session.add(volunteer_user)
 
-    # TODO add users to db
     # TODO add event types to db
     # TODO add events to db (event.event_type = event_type)
 
