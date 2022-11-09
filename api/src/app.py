835e2f9d33523496a39b37dd358e5115656cc39a
Solumath <solumath99@gmail.com>
2022-11-09 16:24:35 +0100

    enable set cookie in user, cors not initialized

--- api/src/app.py
+++ api/src/app.py
@@ -1,5 +1,6 @@
 import flask
 from db import db
+from flask_cors import CORS
 
 
 app = flask.Flask(__name__)
@@ -8,5 +9,5 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://login:password@localhost/iis'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://login:@localhost/iis'
 # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
+CORS(app, supports_credentials=True)
 db.init_app(app)
