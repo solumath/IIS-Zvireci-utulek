aa5c519665d983890f552a8b72efba67a2a75347
Solumath <solumath99@gmail.com>
2022-11-17 19:41:37 +0100

    route to 401, require login, footer to base html

--- routes/routes.py
+++ routes/routes.py
@@ -1,32 +1,38 @@
-from flask import render_template
+import flask
+import flask_login
 from app import app
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return flask.render_template('index.html')
 
 
 @app.route('/animals')
 def animals():
-    return render_template('animals.html')
+    return flask.render_template('animals.html')
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return flask.render_template('about.html')
 
 
 @app.route('/walks')
+@flask_login.login_required
 def walks():
-    return render_template('walks.html')
+    return flask.render_template('walks.html')
 
 
 @app.route('/admin')
+@flask_login.login_required
 def admin():
-    return render_template('admin.html')
+    return flask.render_template('admin.html')
 
+@app.route('/401')
+def unauthorized():
+    return flask.render_template('401.html')
 
-# @app.route('/login')
-# def login_():
-#     return render_template('login.html')
+@app.errorhandler(401)
+def not_enough_perms(e):
+    return flask.render_template('401.html')
