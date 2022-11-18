b817b07c269433a88b3ae089e85c838fbbee8e72
Eduard Frlicka <edo.frlicka@gmail.com>
2022-11-18 21:06:00 +0100

    queries for users

--- db/__init__.py
+++ db/__init__.py
@@ -12,4 +12,4 @@ from .event_type import EventType
 from .animal import Animal
 from .session import Session
 
-from .queries import get_animals, get_past_events
+from .queries import get_animals, get_past_events, get_future_events, get_animal, get_user, get_users
--- db/queries.py
+++ db/queries.py
@@ -6,57 +6,105 @@ from .event_type import EventType
 from datetime import datetime
 
 
-def get_animals(filter: str = None):
+# ====================================================================================================
+# ANIMALS
+
+
+def get_animal(id: int) -> Animal | None:
     """
-        returns serialized array of animals,
-        filter not yet implemented
+        returns animal with id, or None if not found
     """
-    if filter is None:
-        result = db.session.query(Animal).all()
-    else:
-        result = []
+    return db.session.query(Animal).get(id)
+
+
+def get_animals() -> list[Animal]:
+    """
+        returns serialized array of all animals,
+    """
+    result = db.session.query(Animal).all()
+
     return result
 
 
-def get_past_events(user: User | int = None, animal: Animal = None, event_type: EventType = None):
-    """
-        returns serialized array of events,
-        default returns all,
-        if any of arguments is set (user, animal, event_type), filter events by them
-    """
+# ====================================================================================================
+# USERS
 
-    query = db.session.query(Event).filter(Event.end < datetime.now())
 
-    if isinstance(user, int):
-        user = db.session.query(User).get(user)
-        print(user)
-    if isinstance(user, User):
-        query = query.filter(Event.user == user)
-    if animal is not None:
-        query = query.filter(Event.animal == animal)
-    if event_type is not None:
-        query = query.filter(Event.event_type == event_type)
+def get_user(id) -> User | None:
+    """
+        returns user with id, or None if not found
+    """
+    return db.session.query(User).get(id)
+
+
+def get_users(login: str = None, email: str = None, name: str = None, surname: str = None) -> list[User]:
+    """
+        return array of users,
+        if some of the arguments are set, filter by them
+    """
+    query = db.session.query(User)
+
+    if isinstance(login, str):
+        query = query.filter(User.login == login)
+
+    if isinstance(email, str):
+        query = query.filter(User.email == email)
+
+    if isinstance(name, str):
+        query = query.filter(User.name == name)
+
+    if isinstance(surname, str):
+        query = query.filter(User.surname == surname)
 
     return query.all()
 
 
-def get_future_events(user: User | int = None, animal: Animal = None, event_type: EventType = None):
+# ====================================================================================================
+# EVENTS
+
+
+def get_events_query(user: User | int = None, animal: Animal | int = None, event_type: EventType | int = None):
     """
-        returns serialized array of events,
+        returns query for events
+    """
+
+    query = db.session.query(Event)
+
+    if isinstance(user, int):
+        user = db.session.query(User).get(user)
+    if isinstance(user, User):
+        query = query.filter(Event.user == user)
+
+    if isinstance(animal, int):
+        animal = db.session.query(Animal).get(animal)
+    if isinstance(animal, Animal):
+        query = query.filter(Event.animal == animal)
+
+    if isinstance(event_type, int):
+        event_type = db.session.query(EventType).get(event_type)
+    if isinstance(event_type, Animal):
+        query = query.filter(Event.animal == event_type)
+
+    return query
+
+
+def get_past_events(user: User | int = None, animal: Animal | int = None, event_type: EventType | int = None) -> list[Event]:
+    """
+        returns serialized array of events that will end in future,
         default returns all,
         if any of arguments is set (user, animal, event_type), filter events by them
     """
 
-    query = db.session.query(Event).filter(Event.end > datetime.now())
+    query = get_events_query(user, animal, event_type)
+    return query.filter(Event.end < datetime.now()).all()
 
-    if isinstance(user, int):
-        user = db.session.query(User).get(user)
-        print(user)
-    if isinstance(user, User):
-        query = query.filter(Event.user == user)
-    if animal is not None:
-        query = query.filter(Event.animal == animal)
-    if event_type is not None:
-        query = query.filter(Event.event_type == event_type)
 
-    return query.all()
+def get_future_events(user: User | int = None, animal: Animal | int = None, event_type: EventType | int = None) -> list[Event]:
+    """
+        returns serialized array of events that already ended,
+        default returns all,
+        if any of arguments is set (user, animal, event_type), filter events by them
+    """
+
+    query = get_events_query(user, animal, event_type)
+    return query.filter(Event.end < datetime.now()).all()
