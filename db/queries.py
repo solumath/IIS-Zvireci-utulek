a1de3640bd527b5723bfb8ff93f2f00c87b29dda
Eduard Frlicka <edo.frlicka@gmail.com>
2022-11-18 17:56:15 +0100

    future events

--- db/queries.py
+++ db/queries.py
@@ -4,6 +4,7 @@ from .user import User
 from .event import Event
 from .event_type import EventType
 from .schemas import *
+from datetime import datetime
 
 
 def get_animals(filter: str = None):
@@ -26,7 +27,33 @@ def get_past_events(user: User | int = None, animal: Animal = None, event_type: 
         if any of arguments is set (user, animal, event_type), filter events by them
     """
 
-    query = db.session.query(Event)
+    query = db.session.query(Event).filter(Event.end < datetime.now())
+
+    if isinstance(user, int):
+        user = db.session.query(User).get(user)
+        print(user)
+    if isinstance(user, User):
+        query = query.filter(Event.user == user)
+    if animal is not None:
+        query = query.filter(Event.animal == animal)
+    if event_type is not None:
+        query = query.filter(Event.event_type == event_type)
+
+    schema = EventSchema(
+        only=("start", "end", "description", "animal.name", "animal.id"))
+
+    return schema.dump(query.all(), many=True)
+
+
+def get_future_events(user: User | int = None, animal: Animal = None, event_type: EventType = None):
+    """
+        returns serialized array of events,
+        default returns all,
+        if any of arguments is set (user, animal, event_type), filter events by them
+    """
+
+    query = db.session.query(Event).filter(Event.end > datetime.now())
+
     if isinstance(user, int):
         user = db.session.query(User).get(user)
         print(user)
