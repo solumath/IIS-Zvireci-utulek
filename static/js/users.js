7c332bc57eaa22ccc642a90a4f9a4b324bec0a1c
Solumath <solumath99@gmail.com>
2022-11-24 12:46:10 +0100

    search bar not appearing

--- static/js/users.js
+++ static/js/users.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    $('#usersTable').DataTable({
+    $('#usersTableAdmin').DataTable({
         columns: [
             {orderable: false, searchable: true},   // Login
             {orderable: true, searchable: true},    // Role
@@ -14,4 +14,16 @@ $( document ).ready(function() {
             {orderable: false, searchable: false},  // Edit
             {orderable: false, searchable: false}]  // Smazat
       });
+    
+    $('#usersTableCaretaker').DataTable({
+      columns: [
+          {orderable: false, searchable: true},   // Login
+          {orderable: true, searchable: true},    // Role
+          {orderable: true, searchable: true},    // Jméno
+          {orderable: false, searchable: true},   // Bydliště
+          {orderable: true, searchable: true},    // Email
+          {orderable: false, searchable: true},   // Telefon
+          {orderable: false, searchable: true},   // Hodnocení
+          {orderable: true, searchable: true}]    // Ověření
+    });
 });
