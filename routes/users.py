843c78429c2401c84671fc94924953ff64455547
Solumath <solumath99@gmail.com>
2022-11-24 19:43:30 +0100

    fix rendering values in user edit/add, formatting

--- routes/users.py
+++ routes/users.py
@@ -6,46 +6,44 @@ import utility
 import response as r
 
 
-def delete_user(form):
-    if int(form['id']) == flask_login.current_user.id:
-        flask.flash(r.DELETE_YOURSELF_FAILED, r.ERROR)
-        return flask.redirect(flask.url_for('users'))
-
-    user = db.get_user(form['id'])
-
-    for event in db.get_future_events(user):
-        db.db.session.delete(event)
-
-    db.db.session.delete(user)
-    db.db.session.commit()
-    return flask.redirect(flask.url_for('users'))
-
-
 @app.route('/users/delete', methods=['GET', 'POST'])
 @flask_login.login_required
 @utility.role_required(['administrator'])
 def users_delete():
     if flask.request.method == 'POST':
-        return delete_user(flask.request.form)
+        if int(flask.request.form['id']) == flask_login.current_user.id:
+            flask.flash(r.DELETE_YOURSELF_FAILED, r.ERROR)
+            return utility.render_with_permissions(flask.url_for('users'))
+
+        user = db.get_user(flask.request.form['id'])
+        if user is None:
+            flask.flash(r.USER_NOT_FOUND, r.ERROR)
+            return utility.render_with_permissions(flask.url_for('users'))
+
+        for event in db.get_future_events(user):
+            db.db.session.delete(event)
+
+        db.db.session.delete(user)
+        db.db.session.commit()
+
     return flask.redirect(flask.url_for('users'))
 
 
-# TODO admin si nemuze zmenit roli
 @app.route('/users/edit/<id>', methods=['GET', 'POST'])
 @flask_login.login_required
 @utility.role_required(['administrator'])
 def users_edit(id):
     if flask.request.method == "POST":
         user = db.get_user(id)
+        if user is None:
+            flask.flash(r.USER_NOT_FOUND, r.ERROR)
+            return utility.render_with_permissions(flask.url_for('users'))
+
         id = user.id
         user.name = flask.request.form.get('name')
         user.surname = flask.request.form.get('surname')
         user.address = flask.request.form.get('address')
         user.tel_number = flask.request.form.get('tel_number')
-        user.rating = flask.request.form.get('rating')
-        if int(user.rating) < 0:
-            flask.flash(r.NEGATIVE_USER_RATING, r.ERROR)
-            return flask.redirect(flask.url_for('users_edit', id=str(id)))
 
         role_name = flask.request.form.get('user_role')
 
@@ -70,7 +68,7 @@ def user_add():
         # login conflict
         if user_login is not None:
             flask.flash(r.NOT_UNIQUE_LOGIN, r.ERROR)
-            return flask.redirect(flask.url_for('users_edit', id=str(id)))
+            return utility.render_with_permissions('user_add.html', roles=db.get_user_roles())
 
         password = flask.request.form.get("password")
         email = flask.request.form.get("email")
@@ -79,26 +77,22 @@ def user_add():
         # email conflict
         if user_email is not None:
             flask.flash(r.NOT_UNIQUE_EMAIL, r.ERROR)
-            return flask.redirect(flask.url_for('users_edit', id=str(id)))
+            return utility.render_with_permissions('user_add.html', roles=db.get_user_roles())
         name = flask.request.form.get("name")
         surname = flask.request.form.get("surname")
         address = flask.request.form.get("address")
         tel_number = flask.request.form.get("tel_number")
         # TODO check tel. number
-        rating = flask.request.form.get("rating")
-        if (int(rating) < 0):
-            flask.flash(r.NEGATIVE_USER_RATING, r.ERROR)
-            return flask.redirect(flask.url_for('users_edit', id=str(id)))
 
         role_name = flask.request.form.get("user_role")
 
         new_user = db.User(login, password,  name, surname,
-                           address, email, tel_number, rating)
+                           address, email, tel_number)
         db.db.session.add(new_user)
         new_user.user_role = db.get_user_role(role_name)
         db.db.session.commit()
         return flask.redirect(flask.url_for('users'))
-    return utility.render_with_permissions('add_user.html', roles=db.get_user_roles())
+    return utility.render_with_permissions('user_add.html', roles=db.get_user_roles())
 
 
 @app.route('/users/verify/<id>', methods=['POST', 'GET'])
