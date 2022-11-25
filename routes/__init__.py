15b1d5edfd98e2ae9e1fadf40649978de2f25744
Solumath <solumath99@gmail.com>
2022-11-25 23:55:06 +0100

    delete events

--- routes/__init__.py
+++ routes/__init__.py
@@ -4,6 +4,5 @@ from .animals import animals, animals_add, animals_detail, animals_edit
 from .users import users, users_edit, users_delete
 from .walks import walks, walks_delete
 from .my_walks import my_walks, my_walks_delete
-from .events import events, events_delete
 from .examinations import examinations, examinations_delete, examinations_add, examinations_edit
 from .profile import profile, profile_edit, profile_password
