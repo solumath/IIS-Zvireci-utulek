3d8c51f3544e3f16bf1f2efd7ea5ea57ab8ada5c
Solumath <solumath99@gmail.com>
2022-11-25 20:06:59 +0100

    check if not found in db

--- routes/animals.py
+++ routes/animals.py
@@ -15,7 +15,7 @@ def animals_delete():
         animal = db.get_animal(flask.request.form['id'])
         if animal is None:
             flask.flash(r.ANIMAL_NOT_FOUND, r.ERROR)
-            return utility.render_with_permissions(flask.url_for('animals'))
+            return flask.redirect(flask.url_for('animals'))
         for event in animal.events:
             db.db.session.delete(event)
 
@@ -31,47 +31,55 @@ def animals():
 
 @app.route('/animals/<id>')
 def animals_detail(id):
-    return utility.render_with_permissions('animal_detail.html', animal=db.get_animal(id))
+    animal = db.get_animal(id)
+    if animal is None:
+        flask.flash(r.ANIMAL_NOT_FOUND, r.ERROR)
+        return flask.redirect(flask.url_for('animals'))
+    return utility.render_with_permissions('animal_detail.html', animal=animal)
 
 
 @app.route('/animals/add', methods=['POST', 'GET'])
 @flask_login.login_required
 @utility.role_required(['administrator', 'caretaker'])
 def animals_add():
+    if flask.request.method == "POST":
+        name = flask.request.form.get("name")
+        sex = flask.request.form.get("sex")
+        color = flask.request.form.get("color")
+        weight = flask.request.form.get("weight")
 
-    if flask.request.method == "GET":
-        return utility.render_with_permissions('animal_add.html')
+        if (int(weight) < 0):
+            flask.flash(r.NEGATIVE_WEIGHT, r.ERROR)
+            return utility.render_with_permissions('animal_add.html')
+        height = flask.request.form.get("height")
 
-    name = flask.request.form.get("name")
-    sex = flask.request.form.get("sex")
-    color = flask.request.form.get("color")
-    weight = flask.request.form.get("weight")
-    if (int(weight) < 0):
-        flask.flash(r.NEGATIVE_WEIGHT, r.ERROR)
-        return utility.render_with_permissions('animal_add.html', id=str(id))
-    height = flask.request.form.get("height")
-    if (int(height) < 0):
-        flask.flash(r.NEGATIVE_HEIGHT, r.ERROR)
-        return utility.render_with_permissions('animal_add.html', id=str(id))
-    kind = flask.request.form.get("kind")
-    breed = flask.request.form.get("breed")
-    chip_id = flask.request.form.get("chip_id")
-    if (int(chip_id) < 0):
-        flask.flash(r.NEGATIVE_CHIP_ID, r.ERROR)
-        return utility.render_with_permissions('animal_add.html', id=str(id))
-    birthday = utility.parse_date(flask.request.form.get("birthday"))
-    discovery_day = utility.parse_date(flask.request.form.get("discovery_day"))
-    if (discovery_day < birthday):
-        flask.flash(r.WRONG_DISCOVERY_DATE, r.ERROR)
-        return utility.render_with_permissions('animal_add.html', id=str(id))
-    discovery_place = flask.request.form.get("discovery_place")
-    description = flask.request.form.get("description")
+        if (int(height) < 0):
+            flask.flash(r.NEGATIVE_HEIGHT, r.ERROR)
+            return utility.render_with_permissions('animal_add.html')
+        kind = flask.request.form.get("kind")
+        breed = flask.request.form.get("breed")
+        chip_id = flask.request.form.get("chip_id")
 
-    new_animal = db.Animal(name, sex, color, weight, height, kind, breed,
-                           chip_id, birthday, discovery_day, discovery_place, description)
-    db.db.session.add(new_animal)
-    db.db.session.commit()
-    return flask.redirect(f'{new_animal.id}')
+        if (int(chip_id) < 0):
+            flask.flash(r.NEGATIVE_CHIP_ID, r.ERROR)
+            return utility.render_with_permissions('animal_add.html')
+        birthday = utility.parse_date(flask.request.form.get("birthday"))
+        discovery_day = utility.parse_date(flask.request.form.get("discovery_day"))
+
+        if (discovery_day < birthday):
+            flask.flash(r.WRONG_DISCOVERY_DATE, r.ERROR)
+            return utility.render_with_permissions('animal_add.html')
+        discovery_place = flask.request.form.get("discovery_place")
+        description = flask.request.form.get("description")
+
+        new_animal = db.Animal(name, sex, color, weight, height, kind, breed,
+                            chip_id, birthday, discovery_day, discovery_place, description)
+        db.db.session.add(new_animal)
+        db.db.session.commit()
+        return flask.redirect(f'{new_animal.id}')
+    
+    return utility.render_with_permissions('animal_add.html')
+
 
 
 @app.route('/animals/edit/<id>', methods=['GET', 'POST'])
@@ -80,9 +88,11 @@ def animals_add():
 def animals_edit(id):
     if flask.request.method == 'POST':
         animal = db.get_animal(id)
+        if animal is None:
+            flask.flash(r.ANIMAL_NOT_FOUND, r.ERROR)
+            return utility.render_with_permissions(flask.url_for('animals'))
         animal_form = DefaultMunch.fromDict(flask.request.form)
 
-        id = animal.id
         animal.name = flask.request.form.get('name')
         animal.sex = flask.request.form.get('sex')
         animal.color = flask.request.form.get('color')
@@ -90,36 +100,40 @@ def animals_edit(id):
 
         if (int(animal.weight) < 0):
             flask.flash(r.NEGATIVE_WEIGHT, r.ERROR)
-            return utility.render_with_permissions('animal_edit.html', animal=db.get_animal(id))
+            return utility.render_with_permissions('animal_edit.html', animal=animal_form)
         animal.height = flask.request.form.get('height')
 
         if (int(animal.height) < 0):
             flask.flash(r.NEGATIVE_HEIGHT, r.ERROR)
-            return utility.render_with_permissions('animal_edit.html', animal=db.get_animal(id))
+            return utility.render_with_permissions('animal_edit.html', animal=animal_form)
         animal.kind = flask.request.form.get('kind')
         animal.breed = flask.request.form.get('breed')
         animal.chip_id = flask.request.form.get('chip_id')
 
         if (int(animal.chip_id) < 0):
             flask.flash(r.NEGATIVE_CHIP_ID, r.ERROR)
-            return utility.render_with_permissions('animal_edit.html', animal=db.get_animal(id))
+            return utility.render_with_permissions('animal_edit.html', animal=animal_form)
         animal.birthday = utility.parse_date(flask.request.form.get('birthday'))
         animal.discovery_day = utility.parse_date(
             flask.request.form.get('discovery_day'))
 
         if (animal.discovery_day < animal.birthday):
             flask.flash(r.WRONG_DISCOVERY_DATE, r.ERROR)
-            return utility.render_with_permissions('animal_edit.html', animal=db.get_animal(id))
+            return utility.render_with_permissions('animal_edit.html', animal=animal_form)
         animal.discovery_place = flask.request.form.get('discovery_place')
         animal.description = flask.request.form.get('description')
         db.db.session.commit()
-        return flask.redirect(flask.url_for('animals_detail', id=str(id)))
+        return flask.redirect(flask.url_for('animals_detail', id=id))
     
-    return utility.render_with_permissions('animal_edit.html', animal=db.get_animal(id))
+    return utility.render_with_permissions('animal_edit.html', animal=animal)
 
 
 @app.route('/animals/request/<id>', methods=['GET', 'POST'])
 @flask_login.login_required
 @utility.role_required(['administrator', 'caretaker'])
 def medical_request(id):
-    return utility.render_with_permissions('medical_request.html', animal=db.get_animal(id))
+    animal = db.get_animal(id)
+    if animal is None:
+        flask.flash(r.ANIMAL_NOT_FOUND, r.ERROR)
+        return flask.redirect(flask.url_for('animals'))
+    return utility.render_with_permissions('medical_request.html', animal=animal)
