83bc7332e0dc2243d426163a4a004849bc746a1e
Solumath <solumath99@gmail.com>
2022-11-25 21:48:14 +0100

    datatable, sort by confirmed/verify

--- static/js/my_walks.js
+++ static/js/my_walks.js
@@ -1,23 +1,6 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    $('#futureTable').DataTable({
-        columns: [
-            {orderable: true, searchable: false},   // Index
-            {orderable: true, searchable: true},    // Začátek
-            {orderable: true, searchable: true},    // Konec
-            {orderable: true, searchable: true},    // Jméno
-            {orderable: false, searchable: true},   // Popis
-            {orderable: false, searchable: false}]  // Smazat
-    });
-    
-    $('#historyTable').DataTable({
-      columns: [
-            {orderable: false, searchable: true},   // Index
-            {orderable: true, searchable: true},    // Začátek
-            {orderable: true, searchable: true},    // Konec
-            {orderable: true, searchable: true},    // Jméno
-            {orderable: false, searchable: true}]   // Popis
-    });
-
+    $('#futureTable').DataTable();
+    $('#historyTable').DataTable();
 });
--- static/js/users.js
+++ static/js/users.js
@@ -1,29 +1,6 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    $('#usersTableAdmin').DataTable({
-        columns: [
-            {orderable: false, searchable: true},   // Login
-            {orderable: true, searchable: true},    // Role
-            {orderable: true, searchable: true},    // Jméno
-            {orderable: false, searchable: true},   // Bydliště
-            {orderable: true, searchable: true},    // Email
-            {orderable: false, searchable: true},   // Telefon
-            {orderable: false, searchable: true},   // Hodnocení
-            {orderable: true, searchable: true},    // Ověření
-            {orderable: false, searchable: false},  // Edit
-            {orderable: false, searchable: false}]  // Smazat
-      });
-    
-    $('#usersTableCaretaker').DataTable({
-      columns: [
-          {orderable: false, searchable: true},   // Login
-          {orderable: true, searchable: true},    // Role
-          {orderable: true, searchable: true},    // Jméno
-          {orderable: false, searchable: true},   // Bydliště
-          {orderable: true, searchable: true},    // Email
-          {orderable: false, searchable: true},   // Telefon
-          {orderable: false, searchable: true},   // Hodnocení
-          {orderable: true, searchable: true}]    // Ověření
-    });
+    $('.usersTableAdmin').DataTable();
+    $('.usersTableCaretaker').DataTable();
 });
