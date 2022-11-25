ffb9445bfa0ed0e2b1a5ff77de1f00c8d25f1d3f
Pupiennus <nna.krcalova@seznam.cz>
2022-11-25 11:40:24 +0100

    upravene cesty k prohlidkam
    events.py->examinations.py

--- routes/examinations.py
+++ routes/examinations.py
@@ -9,25 +9,29 @@ import utility
 @flask_login.login_required
 @utility.role_required(['administrator', 'caretaker', 'vet'])
 def examinations_add():
-    pass
+    return utility.render_with_permissions('examination_add.html', events=db.get_events_query(), animals=db.get_animals())
 
-
-@app.route('/examinations/edit', methods=['GET', 'POST'])
+@app.route('/examinations/edit/<id>', methods=['GET', 'POST'])
 @flask_login.login_required
 @utility.role_required(['administrator', 'caretaker', 'vet'])
-def examinations_edit():
-    pass
+def examinations_edit(id):
+    return utility.render_with_permissions('examination_edit.html', event=db.get_event(id), events=db.get_events_query())
 
 
 @app.route('/examinations/delete', methods=['GET', 'POST'])
 @flask_login.login_required
 @utility.role_required(['administrator', 'caretaker', 'vet'])
 def examinations_delete():
-    pass
+    if flask.request.method == 'POST':
+        animal = db.get_event(flask.request.form['id'])
+        db.db.session.delete(animal)
+        db.db.session.commit()
+        return flask.redirect('/examinations')
+    return flask.redirect('/examinations')
 
 
 @app.route('/examinations', methods=['GET', 'POST'])
 @flask_login.login_required
 @utility.role_required(['administrator', 'caretaker', 'vet'])
 def examinations():
-    return utility.render_with_permissions('events.html', records=db.get_medical_records())
+    return utility.render_with_permissions('examinations.html', records=db.get_medical_records())
